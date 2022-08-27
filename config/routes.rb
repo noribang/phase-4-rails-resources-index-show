4aabea906b7a472f3cf78031239a877a5ea908cb
hoonkim <hoonk9387@gmail.com>
2022-08-27 13:47:52 -0500

    Created routes with reseources.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '/birds', to: 'birds#index' #Get all birds.
+  # get '/birds/:id', to: 'birds#show' #Get single bird by id.
+
+  # Creates all CRUD RESTful routes.
+  # resources :birds
+
+  # Create only GET routes mapped to index and show actions.
+  resources :birds, only: [:index, :show]
+
 end
