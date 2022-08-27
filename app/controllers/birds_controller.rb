d01c155eed7aac85ceb18a24160c549918c25a1d
hoonkim <hoonk9387@gmail.com>
2022-08-27 15:25:18 -0500

    Completed assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,21 @@
 class BirdsController < ApplicationController
-    
+    # GET /birds
+    # All birds
+    def index
+        # All instances of Bird model 
+        birds = Bird.all
+        render json: birds
+    end
+
+    # GET /birds/:id
+    def show
+        bird = Bird.find_by(id: params[:id])
+        if bird
+            render json: bird
+        else
+            render json: { error: "Bird not found"}, status: :not_found
+        end
+    end
+
 
 end
